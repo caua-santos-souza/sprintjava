7877e9faac1d70c6bb23782bcc8d6c838f1fc2f9
caua dos santos souza <cauadosantosouza@gmail.com>
2025-09-29 23:55:45 -0300

    more fix

--- trackyard/src/main/java/com/mottu/trackyard/TrackyardApplication.java
+++ trackyard/src/main/java/com/mottu/trackyard/TrackyardApplication.java
@@ -4,19 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
-
 @SpringBootApplication
 @EnableCaching
 public class TrackyardApplication {
-    
-    @PostConstruct
-    public void init() {
-        // Configurar timezone para Brasil
-        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
-    }
-    
     public static void main(String[] args) {
         SpringApplication.run(TrackyardApplication.class, args);
     }
